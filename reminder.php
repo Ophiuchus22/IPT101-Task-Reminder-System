2ac99074b2f40941151e99d748d24466a7ea95e0
Michael <darundaymichael4@gmail.com>
2024-05-18 17:03:33 +0800

    Reapply "Merge branch 'main' of https://github.com/Ophiuchus22/IPT101-Task-Reminder-System"
    This reverts commit e69841144f6e9e19a1ba9c08c7d214d2d50d1861.

--- reminder.php
+++ reminder.php
@@ -44,4 +44,22 @@
     <audio id="notificationSound" src="notificationsoundeffect.mp3"></audio>
     <script src="action_script.js"></script>
   </body>
+
+  <div id="editModal" class="modal">
+      <!-- Edit Reminder Modal -->
+      <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2>Edit Reminder</h2>
+        <form id="editForm">
+          <label for="editTitle">Title:</label>
+          <input type="text" id="editTitle" name="title" required><br>
+          <label for="editDescription">Description:</label>
+          <input type="text" id="editDescription" name="description" required><br>
+          <label for="editDateTime">Date and Time (YYYY-MM-DD/HH:MM):</label>
+          <input type="text" id="editDateTime" name="dateTime" required><br>
+          <button type="submit">Save</button>
+        </form>
+      </div>
+    </div>
+    
 </html>
